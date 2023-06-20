cf75e040358d178fc9c1fc0ab54f611674cec965
Mihnea <87712724+cateatingpancakes@users.noreply.github.com>
2023-06-21 02:34:30 +0300

    Paginator update

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import discord
 
+from discord.commands import Option
+from paginator import Paginator
 from scryfall import call
+
+bot = discord.Bot()
+
+@bot.slash_command(name="ping", description="Returns the bot's latency.")
+async def ping(ctx):
+    await ctx.respond(f"Pong! The latency is `{round(bot.latency, 3)}` milliseconds.")
+
+@bot.slash_command(name="search", description="Do a Scryfall search.")
+async def search(ctx, query: Option(str, "The query, as you would write it on the website.", required=True, default="")):
+    if query == "":
+        await ctx.respond("You didn't provide any query!")
+    else:
+        cards = call(query)
+        if cards is None:
+            await ctx.respond("Your query failed due to the Scryfall rate limit, or returned no cards.")
+        else:
+            pass
+            # await ctx.respond(view=Paginator(embeds))
+
+with open("token", encoding="utf-8") as file:
+    token = file.read()
+    bot.run(token)
---
+++ paginator.py
@@ -0,0 +1,23 @@
+import discord
+
+class Paginator(discord.ui.View):
+    def __init__(self, pages: list[discord.Embed]):
+        super().__init__()
+        self.page = 0
+        self.pages = pages
+
+    @discord.ui.button(label="Previous", row=0, style=discord.ButtonStyle.secondary)
+    async def prev_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+        if self.page > 0:
+            self.page -= 1
+            await interaction.response.edit_message(embed=self.pages[self.page])
+        else:
+            pass
+
+    @discord.ui.button(label="Next", row=0, style=discord.ButtonStyle.secondary)
+    async def next_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+        if self.page < len(self.pages) - 1:
+            self.page += 1
+            await interaction.response.edit_message(embed=self.pages[self.page])
+        else:
+            pass
