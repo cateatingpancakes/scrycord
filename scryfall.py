e99768bb7702e93fbbb5b8bbd5fb5b733dbfab33
Mihnea <87712724+cateatingpancakes@users.noreply.github.com>
2023-06-21 15:32:54 +0300

    Removed return type hint

--- scryfall.py
+++ scryfall.py
@@ -4,8 +4,7 @@ from ratelimit import limits
 
 # Scryfall ratelimit
 @limits(calls=10, period=1)
-def call(query: str) -> str:
-
+def call(query: str):
     response = requests.get("https://api.scryfall.com/cards/search?q=" + query)
 
     if response.status_code == 200:
