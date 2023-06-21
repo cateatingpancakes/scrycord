096bcd3aafbdf45ad330a9609e4875a8c1688576
Mihnea <87712724+cateatingpancakes@users.noreply.github.com>
2023-06-21 15:36:24 +0300

    Formatted code

--- paginator.py
+++ paginator.py
@@ -8,9 +8,7 @@ class Paginator(discord.ui.View):
         self.pages = pages
 
     @discord.ui.button(label="Previous", style=discord.ButtonStyle.secondary, row=0)
-    async def previous_callback(
-        self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
+    async def previous_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         if self.page > 0:
             self.page -= 1
             await interaction.response.edit_message(embed=self.pages[self.page])
@@ -18,9 +16,7 @@ class Paginator(discord.ui.View):
             await interaction.response.edit_message(embed=self.pages[0])
 
     @discord.ui.button(label="Next", style=discord.ButtonStyle.secondary, row=0)
-    async def next_callback(
-        self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
+    async def next_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         if self.page < len(self.pages) - 1:
             self.page += 1
             await interaction.response.edit_message(embed=self.pages[self.page])
