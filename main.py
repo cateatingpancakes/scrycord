fcfe0aa835e15991b3d1d37ab19640a4e71ce51a
Mihnea <87712724+cateatingpancakes@users.noreply.github.com>
2023-06-21 15:31:48 +0300

    Formatted code

--- main.py
+++ main.py
@@ -6,21 +6,36 @@ from scryfall import call
 
 bot = discord.Bot()
 
+
 @bot.slash_command(name="ping", description="Returns the bot's latency.")
 async def ping(ctx):
     await ctx.respond(f"Pong! The latency is `{round(bot.latency, 3)}` milliseconds.")
 
+
 @bot.slash_command(name="search", description="Do a Scryfall search.")
-async def search(ctx, query: Option(str, "The query, as you would write it on the website.", required=True, default="")):
-    if query == "":
-        await ctx.respond("You didn't provide any query!")
+async def search(ctx, query: Option(str, "The query, as you would write it on the website.", required=True)):
+    cards = call(query)
+
+    if cards is None:
+        await ctx.respond("Your query failed due to the Scryfall rate limit, or returned no cards.")
     else:
-        cards = call(query)
-        if cards is None:
-            await ctx.respond("Your query failed due to the Scryfall rate limit, or returned no cards.")
-        else:
-            pass
-            # await ctx.respond(view=Paginator(embeds))
+        embeds = []
+        for current, card in enumerate(cards["data"]):
+            total = len(cards["data"])
+
+            embed = discord.Embed(
+                title=card["name"],
+                description=f"Showing result `{current + 1}` of `{total}`.",
+            )
+
+            if "card_faces" in card:
+                card = card["card_faces"][0]
+
+            embed.set_image(url=card["image_uris"]["png"])
+            embeds.append(embed)
+
+        await ctx.respond(embed=embeds[0], view=Paginator(embeds))
+
 
 with open("token", encoding="utf-8") as file:
     token = file.read()
--- paginator.py
+++ paginator.py
@@ -1,23 +1,28 @@
 import discord
 
+
 class Paginator(discord.ui.View):
     def __init__(self, pages: list[discord.Embed]):
         super().__init__()
         self.page = 0
         self.pages = pages
 
-    @discord.ui.button(label="Previous", row=0, style=discord.ButtonStyle.secondary)
-    async def prev_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="Previous", style=discord.ButtonStyle.secondary, row=0)
+    async def previous_callback(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
         if self.page > 0:
             self.page -= 1
-            await interaction.response.edit_message(f"Showing result {self.page} of {self.pages}", embed=self.pages[self.page])
+            await interaction.response.edit_message(embed=self.pages[self.page])
         else:
-            pass
+            await interaction.response.edit_message(embed=self.pages[0])
 
-    @discord.ui.button(label="Next", row=0, style=discord.ButtonStyle.secondary)
-    async def next_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.secondary, row=0)
+    async def next_callback(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
         if self.page < len(self.pages) - 1:
             self.page += 1
-            await interaction.response.edit_message(f"Showing result {self.page} of {self.pages}", embed=self.pages[self.page])
+            await interaction.response.edit_message(embed=self.pages[self.page])
         else:
-            pass
+            await interaction.response.edit_message(embed=self.pages[0])
